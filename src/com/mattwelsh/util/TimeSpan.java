5a3577f92e99eda773ce64310cc86bf6e29857a7
Matt Welsh <mitya.welsh@gmail.com>
2018-12-07 13:26:25 -0800

    Reformat Code

--- src/com/mattwelsh/util/TimeSpan.java
+++ src/com/mattwelsh/util/TimeSpan.java
@@ -3,244 +3,251 @@ package com.mattwelsh.util;
 import java.util.*;
 
 /**
- * This class represents a span of time between two points in time. It uses the JulianDate class to calculate the span
- * length and holds that representation in days. The order the dates are passed is ignored and there are methods to
- * get the span length in days, weeks, months, years, decades and centuries that ignore fractional parts. For example,
- * the span of time between March 7, 2017 at 5:00am and March 7, 2017 at 5:00pm returns '1' for getDays() rather than
- * 1.5 following the convention people most often use when speaking.
+ * This class represents a span of time between two points in time. It uses the JulianDate class to
+ * calculate the span length and holds that representation in days. The order the dates are passed
+ * is ignored and there are methods to get the span length in days, weeks, months, years, decades
+ * and centuries that ignore fractional parts. For example, the span of time between March 7, 2017
+ * at 5:00am and March 7, 2017 at 5:00pm returns '1' for getDays() rather than 1.5 following the
+ * convention people most often use when speaking.
  *
- * If you want the exact span length use getLength() which returns the exact span length as a double.
+ * <p>If you want the exact span length use getLength() which returns the exact span length as a
+ * double.
  *
- * @version 1.0.0
  * @author Matt Welsh July 11, 2017
+ * @version 1.0.0
  */
-
 public class TimeSpan {
 
-    private GregorianCalendar startDateGregorian;
-    private GregorianCalendar endDateGregorian;
-    private JulianDate startDateJulian;
-    private JulianDate endDateJulian;
-    private double length;
-    private boolean forward;
+  private GregorianCalendar startDateGregorian;
+  private GregorianCalendar endDateGregorian;
+  private JulianDate startDateJulian;
+  private JulianDate endDateJulian;
+  private double length;
+  private boolean forward;
 
+  /**
+   * Create a TimeSpan object representing the length of time between startDateGregorian and
+   * endDateGregorian. The order the arguments are passed is irrelevant to the computed length.
+   *
+   * @param startDateGregorian The starting date or the time span.
+   * @param endDateGregorian The ending date of the time span.
+   */
+  public TimeSpan(GregorianCalendar startDateGregorian, GregorianCalendar endDateGregorian) {
 
-    /**
-     * Create a TimeSpan object representing the length of time between startDateGregorian and endDateGregorian. The
-     * order the arguments are passed is irrelevant to the computed length.
-     *
-     * @param startDateGregorian The starting date or the time span.
-     * @param endDateGregorian The ending date of the time span.
-     */
-    public TimeSpan(GregorianCalendar startDateGregorian, GregorianCalendar endDateGregorian) {
+    this.startDateGregorian = startDateGregorian;
+    this.endDateGregorian = endDateGregorian;
+    startDateJulian = new JulianDate(startDateGregorian);
+    endDateJulian = new JulianDate(endDateGregorian);
 
-        this.startDateGregorian = startDateGregorian;
-        this.endDateGregorian = endDateGregorian;
-        startDateJulian = new JulianDate(startDateGregorian);
-        endDateJulian = new JulianDate(endDateGregorian);
+    forward = (startDateJulian.getJulianDayNumber() - endDateJulian.getJulianDayNumber()) >= 0;
+    length = Math.abs(startDateJulian.getJulianDayNumber() - endDateJulian.getJulianDayNumber());
+  }
 
-        forward = (startDateJulian.getJulianDayNumber() - endDateJulian.getJulianDayNumber()) >= 0;
-        length = Math.abs(startDateJulian.getJulianDayNumber() - endDateJulian.getJulianDayNumber());
+  /**
+   * Create a TimeSpan with the given start date and length in days. The length may be negative
+   * resulting in an end date before the start time.
+   *
+   * @param startDateGregorian The start of the time span.
+   * @param length The length of the time span.
+   */
+  public TimeSpan(GregorianCalendar startDateGregorian, double length) {
+
+    this.startDateGregorian = startDateGregorian;
+    this.endDateGregorian = computeEndDate(length);
+    startDateJulian = new JulianDate(startDateGregorian);
+    endDateJulian = new JulianDate(endDateGregorian);
+    this.length =
+        Math.abs(startDateJulian.getJulianDayNumber() - endDateJulian.getJulianDayNumber());
+  }
+
+  /**
+   * Returns the starting date that was used to construct the span.
+   *
+   * @return The start date of the time span.
+   */
+  public GregorianCalendar getStartDateGregorian() {
+    return startDateGregorian;
+  }
+
+  /**
+   * Return the end date used to construct the span.
+   *
+   * @return The end date used to construct the span.
+   */
+  public GregorianCalendar getEndDateGregorian() {
+    return endDateGregorian;
+  }
+
+  /**
+   * Return the exact length of the time span in days as a double.
+   *
+   * @return The exact length of the time span in days as a double.
+   */
+  public double getLength() {
+    return length;
+  }
+
+  /**
+   * Get the number of weeks in the span ignoring fractional parts.
+   *
+   * @return The number of weeks in the span ignoring fractional parts.
+   */
+  public long getWeeks() {
+    return (long) getLength() / 7;
+  }
+
+  /**
+   * Get the number of days in the time span ignoring fractional parts.
+   *
+   * @return The number of days in the time span ignoring fractional parts.
+   */
+  public long getDays() {
+    return (long) getLength();
+  }
+
+  /**
+   * Get the number of years in the time span ignoring fractional parts.
+   *
+   * @return The number of years in the time span ignoring fractional parts.
+   */
+  public long getYears() {
+    return (long) (getLength() / 365.25);
+  }
+
+  /**
+   * Get the number of decades in the time span ignoring fractional parts.
+   *
+   * @return The number of decades in the time span ignoring fractional parts.
+   */
+  public long getDecades() {
+    return getYears() / 10;
+  }
+
+  /**
+   * Get the number of centuries in the time span ignoring fractional parts.
+   *
+   * @return The number of centuries in the time span ignoring fractional parts.
+   */
+  public long getCenturies() {
+    return getDecades() / 10;
+  }
+
+  /**
+   * Get the number of months in the time span ignoring fractional parts.
+   *
+   * @return The number of months in the time span ignoring fractional parts.
+   */
+  public long getMonths() {
+
+    GregorianCalendar g1;
+    GregorianCalendar g2;
+
+    if (forward) {
+      g1 = startDateGregorian;
+      g2 = endDateGregorian;
+    } else {
+      g1 = endDateGregorian;
+      g2 = startDateGregorian;
     }
 
-    /**
-     * Create a TimeSpan with the given start date and length in days. The length may be negative resulting in an end
-     * date before the start time.
-     *
-     * @param startDateGregorian The start of the time span.
-     * @param length The length of the time span.
-     */
-    public TimeSpan(GregorianCalendar startDateGregorian, double length) {
+    long temp = (long) g1.get(GregorianCalendar.YEAR) - (long) g2.get(GregorianCalendar.YEAR);
+    temp = temp * 12;
+    temp += g1.get(GregorianCalendar.MONTH) - g2.get(GregorianCalendar.MONTH);
+    return temp;
+  }
 
-        this.startDateGregorian = startDateGregorian;
-        this.endDateGregorian = computeEndDate(length);
-        startDateJulian = new JulianDate(startDateGregorian);
-        endDateJulian = new JulianDate(endDateGregorian);
-        this.length = Math.abs(startDateJulian.getJulianDayNumber() - endDateJulian.getJulianDayNumber());
+  // ------------------------------------------------------------------------------------------------------------------
+  // Private and protected methods
+  // ------------------------------------------------------------------------------------------------------------------
+
+  private GregorianCalendar computeEndDate(double length) {
+    startDateJulian = new JulianDate(this.startDateGregorian);
+    return computeCalendarDate(this.startDateJulian.getJulianDayNumber() + length);
+  }
+
+  private GregorianCalendar computeCalendarDate(double julianDate) {
+    double startJD = julianDate;
+    startJD += 0.5;
+    double z = getInteger(startJD);
+    double f = getFractional(startJD);
+    double a;
+    double alpha;
+
+    if (z < 2299161) {
+      a = z;
+    } else {
+      alpha = getInteger((z - 1867216.25) / 36524.25);
+      a = z + 1.0 + alpha - getInteger(alpha / 4.0);
     }
 
-    /**
-     * Returns the starting date that was used to construct the span.
-     *
-     * @return The start date of the time span.
-     */
-    public GregorianCalendar getStartDateGregorian() {
-        return startDateGregorian;
+    double b = a + 1524;
+    double c = getInteger((b - 122.1) / 365.25);
+    double d = getInteger(365.25 * c);
+    double e = getInteger((b - d) / 30.6001);
+
+    double day = b - d - (long) (30.6001 * e) + f;
+
+    double month;
+    if (e < 14) {
+      month = e - 1;
+    } else {
+      month = e - 13;
     }
 
-    /**
-     * Return the end date used to construct the span.
-     *
-     * @return The end date used to construct the span.
-     */
-    public GregorianCalendar getEndDateGregorian() {
-        return endDateGregorian;
+    double year;
+    if (month > 2) {
+      year = c - 4716;
+    } else {
+      year = c - 4715;
     }
 
-    /**
-     * Return the exact length of the time span in days as a double.
-     *
-     * @return The exact length of the time span in days as a double.
-     */
-    public double getLength() {
-        return length;
-    }
+    double hours = getFractional(day);
+    hours = hours * 24;
+    day = getInteger(day);
 
-    /**
-     * Get the number of weeks in the span ignoring fractional parts.
-     *
-     * @return The number of weeks in the span ignoring fractional parts.
-     */
-    public long getWeeks() {
-        return (long) getLength() / 7;
-    }
+    double minutes = getFractional(hours);
+    minutes = minutes * 60;
 
-    /**
-     * Get the number of days in the time span ignoring fractional parts.
-     *
-     * @return The number of days in the time span ignoring fractional parts.
-     */
-    public long getDays() {
-      return (long) getLength();
-    }
+    double seconds = getFractional(minutes);
+    minutes = getInteger(minutes);
+    seconds = seconds * 60;
 
-    /**
-     * Get the number of years in the time span ignoring fractional parts.
-     *
-     * @return The number of years in the time span ignoring fractional parts.
-     */
-    public long getYears() {
-        return (long)(getLength() /365.25);
-    }
+    double milliseconds = getFractional(seconds);
+    seconds = getInteger(seconds);
+    milliseconds *= 1000;
 
-    /**
-     * Get the number of decades in the time span ignoring fractional parts.
-     *
-     * @return The number of decades in the time span ignoring fractional parts.
-     */
-    public long getDecades() {
-        return getYears() / 10;
-    }
+    GregorianCalendar greg =
+        new GregorianCalendar(
+            (int) year, (int) month, (int) day, (int) hours, (int) minutes, (int) seconds);
 
-    /**
-     * Get the number of centuries in the time span ignoring fractional parts.
-     *
-     * @return The number of centuries in the time span ignoring fractional parts.
-     */
-    public long getCenturies() {
-        return getDecades()/10;
-    }
+    greg.set(Calendar.MILLISECOND, (int) milliseconds);
+    return greg;
+  }
 
-    /**
-     * Get the number of months in the time span ignoring fractional parts.
-     *
-     * @return The number of months in the time span ignoring fractional parts.
-     */
-    public long getMonths() {
+  private double getFractional(double d) {
+    return d - getInteger(d);
+  }
 
-        GregorianCalendar g1;
-        GregorianCalendar g2;
+  private double getInteger(double d) {
+    return (double) ((long) d);
+  }
 
-        if(forward) {
-            g1 = startDateGregorian;
-            g2 = endDateGregorian;
-        }
-        else {
-            g1 = endDateGregorian;
-            g2 = startDateGregorian;
-        }
+  private void printGregorianCalendar(GregorianCalendar greg) {
 
-        long temp = (long) g1.get(GregorianCalendar.YEAR) - (long) g2.get(GregorianCalendar.YEAR);
-        temp = temp * 12;
-        temp += g1.get(GregorianCalendar.MONTH) - g2.get(GregorianCalendar.MONTH);
-        return temp;
-    }
-
-    //------------------------------------------------------------------------------------------------------------------
-    // Private and protected methods
-    //------------------------------------------------------------------------------------------------------------------
-
-    private GregorianCalendar computeEndDate(double length) {
-        startDateJulian = new JulianDate(this.startDateGregorian);
-        return computeCalendarDate(this.startDateJulian.getJulianDayNumber() + length);
-    }
-
-    private GregorianCalendar computeCalendarDate(double julianDate) {
-        double startJD = julianDate;
-        startJD += 0.5;
-        double z = getInteger(startJD);
-        double f = getFractional(startJD);
-        double a;
-        double alpha;
-
-        if(z < 2299161) {
-            a = z;
-        }
-        else {
-            alpha = getInteger((z-1867216.25)/36524.25);
-            a = z + 1.0 + alpha - getInteger(alpha / 4.0);
-        }
-
-        double b = a + 1524;
-        double c = getInteger((b-122.1)/365.25);
-        double d = getInteger(365.25 * c);
-        double e = getInteger((b-d)/30.6001);
-
-        double day = b - d - (long)(30.6001 * e) + f;
-
-        double month;
-        if(e < 14) {
-            month = e - 1;
-        } else {
-            month = e - 13;
-        }
-
-        double year;
-        if (month > 2) {
-            year = c - 4716;
-        }
-        else {
-            year = c - 4715;
-        }
-
-        double hours = getFractional(day);
-        hours = hours * 24;
-        day = getInteger(day);
-
-        double minutes = getFractional(hours);
-        minutes = minutes * 60;
-
-        double seconds = getFractional(minutes);
-        minutes = getInteger(minutes);
-        seconds = seconds *60;
-
-        double milliseconds = getFractional(seconds);
-        seconds = getInteger(seconds);
-        milliseconds *= 1000;
-
-        GregorianCalendar greg =  new GregorianCalendar((int)year, (int)month, (int)day,
-                                    (int)hours, (int)minutes, (int)seconds);
-
-        greg.set(Calendar.MILLISECOND, (int)milliseconds);
-        return greg;
-
-    }
-
-    private double getFractional(double d) {
-        return d - getInteger(d);
-    }
-
-    private double getInteger(double d) {
-        return (double)((long)d);
-    }
-
-    private void printGregorianCalendar(GregorianCalendar greg) {
-
-        System.out.println("Computed Date: " + greg.get(Calendar.MONTH) + "/" + greg.get(Calendar.DAY_OF_MONTH) + "/" +
-                            greg.get(Calendar.YEAR) + " at " + greg.get(Calendar.HOUR_OF_DAY) + ":" +
-                            greg.get(Calendar.MINUTE) + ":" + greg.get(Calendar.SECOND) + ":" +
-                            greg.get(Calendar.MILLISECOND));
-
-    }
-
+    System.out.println(
+        "Computed Date: "
+            + greg.get(Calendar.MONTH)
+            + "/"
+            + greg.get(Calendar.DAY_OF_MONTH)
+            + "/"
+            + greg.get(Calendar.YEAR)
+            + " at "
+            + greg.get(Calendar.HOUR_OF_DAY)
+            + ":"
+            + greg.get(Calendar.MINUTE)
+            + ":"
+            + greg.get(Calendar.SECOND)
+            + ":"
+            + greg.get(Calendar.MILLISECOND));
+  }
 }
--- test/com/mattwelsh/util/TimeSpanTest.java
+++ test/com/mattwelsh/util/TimeSpanTest.java
@@ -8,121 +8,110 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * Tests the Time Span Class
- * Created by Matt on 7/15/17.
- */
+/** Tests the Time Span Class Created by Matt on 7/15/17. */
 public class TimeSpanTest {
 
-    @Before
-    public void setUp() throws Exception {
+  @Before
+  public void setUp() throws Exception {}
 
-    }
+  @After
+  public void tearDown() throws Exception {}
 
-    @After
-    public void tearDown() throws Exception {
+  @Test
+  public void getStartDateGregorian() throws Exception {
+    GregorianCalendar cal1 = new GregorianCalendar(2001, 5, 1);
+    TimeSpan span2 = new TimeSpan(cal1, new GregorianCalendar(2001, 5, 18));
+    Assert.assertEquals(span2.getStartDateGregorian(), cal1);
 
-    }
+    cal1 = new GregorianCalendar(1964, 5, 1);
+    span2 = new TimeSpan(cal1, new GregorianCalendar(2014, 6, 18));
+    Assert.assertEquals(span2.getStartDateGregorian(), cal1);
 
-    @Test
-    public void getStartDateGregorian() throws Exception {
-        GregorianCalendar cal1 = new GregorianCalendar(2001, 5, 1);
-        TimeSpan span2 = new TimeSpan(cal1, new GregorianCalendar(2001, 5, 18));
-        Assert.assertEquals(span2.getStartDateGregorian(), cal1);
+    cal1 = new GregorianCalendar(2014, 1, 1);
+    span2 = new TimeSpan(cal1, new GregorianCalendar(2014, 6, 18));
+    Assert.assertEquals(span2.getStartDateGregorian(), cal1);
 
-        cal1 = new GregorianCalendar(1964, 5, 1);
-        span2 = new TimeSpan(cal1, new GregorianCalendar(2014, 6, 18));
-        Assert.assertEquals(span2.getStartDateGregorian(), cal1);
+    cal1 = new GregorianCalendar();
+    span2 = new TimeSpan(cal1, new GregorianCalendar(2014, 6, 18));
+    Assert.assertEquals(span2.getStartDateGregorian(), cal1);
+  }
 
-        cal1 = new GregorianCalendar(2014, 1, 1);
-        span2 = new TimeSpan(cal1, new GregorianCalendar(2014, 6, 18));
-        Assert.assertEquals(span2.getStartDateGregorian(), cal1);
+  @Test
+  public void getEndDateGregorian() throws Exception {
+    GregorianCalendar cal1 = new GregorianCalendar(2001, 5, 1);
+    TimeSpan span2 = new TimeSpan(new GregorianCalendar(2001, 5, 18), cal1);
+    Assert.assertEquals(span2.getEndDateGregorian(), cal1);
 
-        cal1 = new GregorianCalendar();
-        span2 = new TimeSpan(cal1, new GregorianCalendar(2014, 6, 18));
-        Assert.assertEquals(span2.getStartDateGregorian(), cal1);
-    }
+    cal1 = new GregorianCalendar(1964, 5, 1);
+    span2 = new TimeSpan(new GregorianCalendar(2014, 6, 18), cal1);
+    Assert.assertEquals(span2.getEndDateGregorian(), cal1);
 
-    @Test
-    public void getEndDateGregorian() throws Exception {
-        GregorianCalendar cal1 = new GregorianCalendar(2001, 5, 1);
-        TimeSpan span2 = new TimeSpan(new GregorianCalendar(2001, 5, 18), cal1);
-        Assert.assertEquals(span2.getEndDateGregorian(), cal1);
+    cal1 = new GregorianCalendar(2014, 6, 18);
+    span2 = new TimeSpan(new GregorianCalendar(2014, 1, 1), new GregorianCalendar(2014, 6, 18));
+    Assert.assertEquals(span2.getEndDateGregorian(), cal1);
 
-        cal1 = new GregorianCalendar(1964, 5, 1);
-        span2 = new TimeSpan(new GregorianCalendar(2014, 6, 18), cal1);
-        Assert.assertEquals(span2.getEndDateGregorian(), cal1);
+    cal1 = new GregorianCalendar(2014, 1, 2);
+    span2 = new TimeSpan(new GregorianCalendar(2014, 1, 1), 1.0);
+    Assert.assertEquals(span2.getEndDateGregorian(), cal1);
+  }
 
-        cal1 = new GregorianCalendar(2014, 6, 18);
-        span2 = new TimeSpan(new GregorianCalendar(2014, 1, 1), new GregorianCalendar(2014, 6, 18));
-        Assert.assertEquals(span2.getEndDateGregorian(), cal1);
+  @Test
+  public void getLength() throws Exception {
 
-        cal1 = new GregorianCalendar(2014, 1, 2);
-        span2 = new TimeSpan(new GregorianCalendar(2014, 1, 1), 1.0);
-        Assert.assertEquals(span2.getEndDateGregorian(), cal1);
-    }
+    TimeSpan span2 = new TimeSpan(new GregorianCalendar(2014, 1, 1), 1.0);
+    Assert.assertEquals(span2.getLength(), 1.0, 0.0);
 
-    @Test
-    public void getLength() throws Exception {
+    span2 = new TimeSpan(new GregorianCalendar(2011, 2, 14), new GregorianCalendar(2012, 2, 14));
+    Assert.assertEquals(span2.getLength(), 365.0, 0.0);
+  }
 
-        TimeSpan span2 = new TimeSpan(new GregorianCalendar(2014, 1, 1), 1.0);
-        Assert.assertEquals(span2.getLength(), 1.0, 0.0);
+  @Test
+  public void getWeeks() throws Exception {
+    TimeSpan span2 = new TimeSpan(new GregorianCalendar(2014, 1, 1), 7.0);
+    Assert.assertEquals(span2.getLength(), 7.0, 0.0);
 
-        span2 = new TimeSpan(new GregorianCalendar(2011, 2, 14), new GregorianCalendar(2012, 2, 14));
-        Assert.assertEquals(span2.getLength(), 365.0, 0.0);
+    span2 = new TimeSpan(new GregorianCalendar(2014, 1, 1), new GregorianCalendar(2014, 1, 7));
+    Assert.assertEquals(span2.getLength(), 6.0, 0.0);
+  }
 
-    }
+  @Test
+  public void getDays() throws Exception {
+    TimeSpan span2 = new TimeSpan(new GregorianCalendar(2014, 1, 1), 1.5);
+    Assert.assertEquals(span2.getDays(), 1, 0);
 
-    @Test
-    public void getWeeks() throws Exception {
-        TimeSpan span2 = new TimeSpan(new GregorianCalendar(2014, 1, 1), 7.0);
-        Assert.assertEquals(span2.getLength(), 7.0, 0.0);
+    span2 = new TimeSpan(new GregorianCalendar(2011, 2, 14), new GregorianCalendar(2012, 2, 14));
+    Assert.assertEquals(span2.getDays(), 365.0, 0);
+  }
 
-        span2 = new TimeSpan(new GregorianCalendar(2014, 1, 1), new GregorianCalendar(2014, 1, 7));
-        Assert.assertEquals(span2.getLength(), 6.0, 0.0);
+  @Test
+  public void getYears() throws Exception {
+    TimeSpan span2 = new TimeSpan(new GregorianCalendar(2014, 1, 1), 378.5);
+    Assert.assertEquals(span2.getYears(), 1, 0);
 
-    }
+    span2 = new TimeSpan(new GregorianCalendar(2011, 2, 14), new GregorianCalendar(2017, 3, 14));
+    Assert.assertEquals(span2.getYears(), 6, 0);
+  }
 
-    @Test
-    public void getDays() throws Exception {
-        TimeSpan span2 = new TimeSpan(new GregorianCalendar(2014, 1, 1), 1.5);
-        Assert.assertEquals(span2.getDays(), 1, 0);
+  @Test
+  public void getDecades() throws Exception {
+    TimeSpan span2 =
+        new TimeSpan(new GregorianCalendar(1957, 5, 18), new GregorianCalendar(2007, 5, 19));
+    Assert.assertEquals(span2.getDecades(), 5, 0);
+  }
 
-        span2 = new TimeSpan(new GregorianCalendar(2011, 2, 14), new GregorianCalendar(2012, 2, 14));
-        Assert.assertEquals(span2.getDays(), 365.0, 0);
+  @Test
+  public void getCenturies() throws Exception {
+    TimeSpan span2 =
+        new TimeSpan(new GregorianCalendar(1857, 5, 18), new GregorianCalendar(2057, 5, 19));
+    Assert.assertEquals(span2.getCenturies(), 2, 0);
+  }
 
-    }
+  @Test
+  public void getMonths() throws Exception {
+    TimeSpan span2 = new TimeSpan(new GregorianCalendar(2014, 1, 1), 378.5);
+    Assert.assertEquals(span2.getMonths(), 12, 0);
 
-    @Test
-    public void getYears() throws Exception {
-        TimeSpan span2 = new TimeSpan(new GregorianCalendar(2014, 1, 1), 378.5);
-        Assert.assertEquals(span2.getYears(), 1, 0);
-
-        span2 = new TimeSpan(new GregorianCalendar(2011, 2, 14), new GregorianCalendar(2017, 3, 14));
-        Assert.assertEquals(span2.getYears(), 6, 0);
-
-    }
-
-    @Test
-    public void getDecades() throws Exception {
-        TimeSpan span2 = new TimeSpan(new GregorianCalendar(1957, 5, 18), new GregorianCalendar(2007, 5, 19));
-        Assert.assertEquals(span2.getDecades(), 5, 0);
-    }
-
-    @Test
-    public void getCenturies() throws Exception {
-        TimeSpan span2 = new TimeSpan(new GregorianCalendar(1857, 5, 18), new GregorianCalendar(2057, 5, 19));
-        Assert.assertEquals(span2.getCenturies(), 2, 0);
-    }
-
-    @Test
-    public void getMonths() throws Exception {
-        TimeSpan span2 = new TimeSpan(new GregorianCalendar(2014, 1, 1), 378.5);
-        Assert.assertEquals(span2.getMonths(), 12, 0);
-
-        span2 = new TimeSpan(new GregorianCalendar(1957, 2, 14), new GregorianCalendar(2057, 2, 15));
-        Assert.assertEquals(span2.getMonths(), 1200, 0);
-
-    }
-
+    span2 = new TimeSpan(new GregorianCalendar(1957, 2, 14), new GregorianCalendar(2057, 2, 15));
+    Assert.assertEquals(span2.getMonths(), 1200, 0);
+  }
 }
