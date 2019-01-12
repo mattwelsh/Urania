d5a3141f505ef7418e28a355d4d1cbce994c281b
Matt Welsh <mitya.welsh@gmail.com>
2019-01-11 19:03:43 -0800

    Comments

--- src/com/mattwelsh/astronomy/time/ChaprontDeltaT.java
+++ src/com/mattwelsh/astronomy/time/ChaprontDeltaT.java
@@ -10,7 +10,6 @@
  * Lesser General Public License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
-
 package com.mattwelsh.astronomy.time;
 
 import com.mattwelsh.util.JulianDate;
