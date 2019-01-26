95df0ea15b602dff7544e544388ca6735ddfe159
Matt Welsh <mitya.welsh@gmail.com>
2019-01-25 19:41:15 -0800

    Site commit

--- src/com/mattwelsh/astronomy/observer/Site.java
+++ src/com/mattwelsh/astronomy/observer/Site.java
@@ -10,6 +10,7 @@
  * Lesser General Public License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 U.S.A.
  */
+
 package com.mattwelsh.astronomy.observer;
 
 /**
--- test/com/mattwelsh/astronomy/observer/SiteTest.java
+++ test/com/mattwelsh/astronomy/observer/SiteTest.java
@@ -1,6 +1,5 @@
 package com.mattwelsh.astronomy.observer;
 
-
 import org.junit.Before;
 import org.junit.Test;
 
