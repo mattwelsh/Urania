263625941fe3e00c0d64a539532f45e9a6d50021
Matt Welsh <mitya.welsh@gmail.com>
2019-04-06 14:44:34 -0700

    VSOPb Files for Mars

---
+++ src/com/mattwelsh/astronomy/object/VSOP87b/PlanetaryComputer.java
@@ -0,0 +1,66 @@
+/*
+ *  Copyright (C) 2019 by Matt Welsh
+ *  This library is free software; you can redistribute it and/or modify it under the terms of the
+ *  GNU Lesser General Public License as published by the Free Software Foundation; either version
+ *  2.1 of the License, or any later version.
+ *
+ *  This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU General Public License for more details. You should have received a copy of the GNU
+ *  Lesser General Public License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+package com.mattwelsh.astronomy.object.VSOP87b;
+
+import com.mattwelsh.astronomy.time.JulianDate;
+
+public abstract class PlanetaryComputer {
+
+  /**
+   * Initializes a computer using the passed JulianDate.
+   *
+   * @param jd The JulianDate to use to initialize this object.
+   */
+  public PlanetaryComputer(JulianDate jd) {
+    setJulianDate(jd);
+  }
+
+  protected LbrDataReader dataReader;
+  protected JulianDate julianDate;
+  protected double t;
+  protected double heliocentricLongitude;
+  protected double heliocentricLatitude;
+  protected double radiusVector;
+
+  /**
+   * Sets the julian date that the computer uses for calculations. Calling this method will trigger
+   * a recalculation of all of it's fields.
+   *
+   * @param jd The julian date that the computer uses for calculations.
+   */
+  public void setJulianDate(JulianDate jd) {
+    this.julianDate = jd;
+    this.t = computeT(julianDate.getJulianDayNumber());
+    initializeDataReader();
+    updateFields();
+  }
+
+  protected double computeT(double jDN) {
+    return (jDN - 2451545.0) / 365250.0;
+  }
+
+  protected abstract void initializeDataReader();
+
+  // -----------------------------------------------------------------------------------------------
+  // Protected, package local, & and private methods
+  // -----------------------------------------------------------------------------------------------
+
+  protected void updateFields() {
+    this.t = computeT(julianDate.getJulianDayNumber());
+    this.heliocentricLongitude = dataReader.getHeliocentricLongitude();
+    this.heliocentricLatitude = dataReader.getHeliocentricLatitude();
+    this.radiusVector = dataReader.getRadiusVector();
+  }
+
+}
